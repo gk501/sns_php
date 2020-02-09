a70a5f48b8b4c09ea0c7d84d1ac2c98269aefa6b
gk501 <kishino@cassini.co.jp>
2020-02-09 17:12:48 +0900

    lesson 6

---
+++ lib/Controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MyApp\Controller;
+
+class Index extends \MyApp\Controller {
+
+  public function run() {
+    if (!$this->isLoggedIn()) {
+      // login
+    }
+
+    // get users info
+  }
+  
+}
