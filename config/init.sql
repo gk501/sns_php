03107e0b16afd16181104d7194b653b230b773ed
gk501 <kishino@cassini.co.jp>
2020-02-09 16:12:16 +0900

    lesson 2:

---
+++ config/init.sql
@@ -0,0 +1,15 @@
+create database dotinstall_sns_php;
+
+grant all on dotinstall_sns_php.* to dbuser@localhost identified by 'dotinstall_sns_php_pass';
+
+use dotinstall_sns_php;
+
+create table users (
+  id int not null auto_increment primary key,
+  email varchar(255) unique,
+  password varchar(255),
+  created datetime,
+  modified detetime
+);
+
+desc users;
