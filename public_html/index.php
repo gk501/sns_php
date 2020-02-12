2c3e24ad7ddd61002862511fd0c1b80d614586d5
gk501 <kishino@cassini.co.jp>
2020-02-12 21:32:57 +0900

    lesson 24

--- public_html/index.php
+++ public_html/index.php
@@ -6,9 +6,9 @@ require_once(__DIR__ . '/../config/config.php');
 
 // var_dump($_SESSION['me']);
 
-// $app = new MyApp\Controller\Index();
+$app = new MyApp\Controller\Index();
 
-// $app->run();
+$app->run();
 
 ?>
 <!DOCTYPE html>
